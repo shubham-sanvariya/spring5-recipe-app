8a79dc9efca0a215ec83935e349e3a1c2edc2e9e
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-20 23:21:06 +0530

    adding UnitOfMeasure

---
+++ src/main/java/shubh/springFramework/domain/UnitOfMeasure.java
@@ -0,0 +1,28 @@
+package shubh.springFramework.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id // id value for the database
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
