c374897fd0ec9325ff9afb4088bcb08b0a6d05da
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-26 13:18:22 +0530

    adding Repo's and getting id by passing String value

--- src/main/java/shubh/springFramework/controllers/IndexController.java
+++ src/main/java/shubh/springFramework/controllers/IndexController.java
@@ -2,12 +2,35 @@ package shubh.springFramework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shubh.springFramework.domain.Category;
+import shubh.springFramework.domain.UnitOfMeasure;
+import shubh.springFramework.repositories.CategoryRepository;
+import shubh.springFramework.repositories.UnitOfMeasureRepository;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+
+
     @RequestMapping({"","/","/index"})
     public String getindexPage() {
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        System.out.println("category id is " + categoryOptional.get().getId());
+        System.out.println("UOM id is " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
