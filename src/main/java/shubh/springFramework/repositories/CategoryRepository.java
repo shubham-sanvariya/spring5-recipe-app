58192c8c844c171697485002539be6a78c13a102
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-26 13:17:14 +0530

    setting Optional findByDescription in Repo's

--- src/main/java/shubh/springFramework/repositories/CategoryRepository.java
+++ src/main/java/shubh/springFramework/repositories/CategoryRepository.java
@@ -3,5 +3,8 @@ package shubh.springFramework.repositories;
 import org.springframework.data.repository.CrudRepository;
 import shubh.springFramework.domain.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
+    Optional<Category> findByDescription (String description);
 }
--- src/main/java/shubh/springFramework/repositories/UnitOfMeasureRepository.java
+++ src/main/java/shubh/springFramework/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package shubh.springFramework.repositories;
 import org.springframework.data.repository.CrudRepository;
 import shubh.springFramework.domain.UnitOfMeasure;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+    Optional<UnitOfMeasure> findByDescription (String description);
+
 }
