eab4270e4d49006f9ec2d884b51e6e0dabf83e93
shubham-sanvariya <shubhamsanvariyait.com>
2023-02-27 13:57:25 +0530

    updating recipe by adding Lob and HashSet

--- src/main/java/shubh/springFramework/domain/Recipe.java
+++ src/main/java/shubh/springFramework/domain/Recipe.java
@@ -18,6 +18,7 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+    @Lob
     private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
@@ -35,7 +36,7 @@ public class Recipe {
     @JoinTable(name = "recipe_category",
                 joinColumns = @JoinColumn(name = "recipe_id"),
                 inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     public Long getId() {
         return id;
