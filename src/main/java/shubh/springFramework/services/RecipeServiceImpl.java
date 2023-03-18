4c7e469e364e7c696523db08bfe630a45438eb20
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-18 14:55:47 +0530

    adding RecipeCommand & implementing method

--- src/main/java/shubh/springFramework/services/RecipeServiceImpl.java
+++ src/main/java/shubh/springFramework/services/RecipeServiceImpl.java
@@ -2,6 +2,10 @@ package shubh.springFramework.services;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shubh.springFramework.commands.RecipeCommand;
+import shubh.springFramework.converters.RecipeCommandToRecipe;
+import shubh.springFramework.converters.RecipeToRecipeCommand;
 import shubh.springFramework.domain.Recipe;
 import shubh.springFramework.repositories.RecipeRepository;
 
@@ -13,9 +17,13 @@ import java.util.Set;
 @Service
 public class RecipeServiceImpl implements RecipeService {
     private final RecipeRepository recipeRepository;
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
-    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
 
     @Override
@@ -35,6 +43,17 @@ public class RecipeServiceImpl implements RecipeService {
             throw new RuntimeException("Recipe Not Found");
         }
         return recipeOptional.get();
+    }
+
+    @Override
+    @Transactional
+    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+        Recipe detachedRecipe = recipeCommandToRecipe.convert(command);
+
+        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+        log.debug("Saved RecipeId:" + savedRecipe.getId());
+        return recipeToRecipeCommand.convert(savedRecipe);
+
     }
 
 }
