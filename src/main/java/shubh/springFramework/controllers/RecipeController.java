75260f5c73cba3c58b9cd9940e05e655e57c621b
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-23 11:53:50 +0530

    adding mapping for new recipe

--- src/main/java/shubh/springFramework/controllers/RecipeController.java
+++ src/main/java/shubh/springFramework/controllers/RecipeController.java
@@ -2,15 +2,17 @@ package shubh.springFramework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shubh.springFramework.commands.RecipeCommand;
 import shubh.springFramework.services.RecipeService;
 
 @Controller
 public class RecipeController {
 
     private final RecipeService recipeService;
-
     public RecipeController(RecipeService recipeService) {
         this.recipeService = recipeService;
     }
@@ -18,8 +20,23 @@ public class RecipeController {
     @RequestMapping("/recipe/show/{id}")
     public String showById(@PathVariable String id, Model model){
 
-        model.addAttribute("recipe",recipeService.findById(Long.parseLong(id)));
-
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
     }
+
+    @RequestMapping("recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @PostMapping("recipe")
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command){
+        System.out.println("in post");
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
+    }
+
 }
