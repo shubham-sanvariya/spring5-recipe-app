eefd502e2a4f06d66bd7c24e186b6028bff07ad6
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-24 15:20:37 +0530

    adding test getRecipeCommandByIdTest

--- src/test/java/shubh/springFramework/services/RecipeServiceImplTest.java
+++ src/test/java/shubh/springFramework/services/RecipeServiceImplTest.java
@@ -4,6 +4,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import shubh.springFramework.commands.RecipeCommand;
 import shubh.springFramework.converters.RecipeCommandToRecipe;
 import shubh.springFramework.converters.RecipeToRecipeCommand;
 import shubh.springFramework.domain.Recipe;
@@ -45,6 +46,27 @@ public class RecipeServiceImplTest {
         Recipe recipeReturned = recipeService.findById(1L);
 
         assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+
+    @Test
+    public void getRecipeCommandByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        when(recipeToRecipeCommand.convert(any())).thenReturn(recipeCommand);
+
+        RecipeCommand commandById = recipeService.findCommandById(1L);
+
+        assertNotNull("Null recipe returned", commandById);
         verify(recipeRepository, times(1)).findById(anyLong());
         verify(recipeRepository, never()).findAll();
     }
