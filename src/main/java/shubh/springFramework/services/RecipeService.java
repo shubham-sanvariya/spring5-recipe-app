a9c1a3856df5595247569cd1af42f33394fecf2c
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-26 13:25:48 +0530

    adding method deleteById in Service

--- src/main/java/shubh/springFramework/services/RecipeService.java
+++ src/main/java/shubh/springFramework/services/RecipeService.java
@@ -15,4 +15,6 @@ public interface RecipeService {
     RecipeCommand findCommandById(Long l);
 
     RecipeCommand saveRecipeCommand(RecipeCommand command);
+
+    void deleteById(Long idToDelete);
 }
