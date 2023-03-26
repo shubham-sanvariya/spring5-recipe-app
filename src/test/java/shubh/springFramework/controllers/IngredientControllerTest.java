6673365950f8432e7ea690e6eb092455f54bceb4
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-26 15:18:16 +0530

    adding test to show Ingredient

--- src/test/java/shubh/springFramework/controllers/IngredientControllerTest.java
+++ src/test/java/shubh/springFramework/controllers/IngredientControllerTest.java
@@ -6,7 +6,9 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import shubh.springFramework.commands.IngredientCommand;
 import shubh.springFramework.commands.RecipeCommand;
+import shubh.springFramework.services.IngredientService;
 import shubh.springFramework.services.RecipeService;
 
 
@@ -23,6 +25,8 @@ public class IngredientControllerTest {
 
 
     @Mock
+    IngredientService ingredientService;
+    @Mock
     RecipeService recipeService;
 
     IngredientController controller;
@@ -33,7 +37,7 @@ public class IngredientControllerTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
-        controller = new IngredientController( recipeService);
+        controller = new IngredientController( recipeService, ingredientService);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
     }
 
@@ -51,6 +55,21 @@ public class IngredientControllerTest {
 
         //then
         verify(recipeService, times(1)).findCommandById(anyLong());
+    }
+
+    @Test
+    public void testShowIngredient() throws Exception {
+        //given
+        IngredientCommand ingredientCommand = new IngredientCommand();
+
+        //when
+        when(ingredientService.findByRecipeIdAndIngredientId(anyLong(), anyLong())).thenReturn(ingredientCommand);
+
+        //then
+        mockMvc.perform(get("/recipe/1/ingredient/2/show"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/show"))
+                .andExpect(model().attributeExists("ingredient"));
     }
 
 }
