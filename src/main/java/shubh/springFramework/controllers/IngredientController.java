35fe3eb79b022afc54b91f2f4e75e1e67d503b6d
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-26 15:16:26 +0530

    adding requestMapping for a ingredient to view

--- src/main/java/shubh/springFramework/controllers/IngredientController.java
+++ src/main/java/shubh/springFramework/controllers/IngredientController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import shubh.springFramework.services.IngredientService;
 import shubh.springFramework.services.RecipeService;
 
 @Slf4j
@@ -13,9 +14,11 @@ import shubh.springFramework.services.RecipeService;
 public class IngredientController {
 
     private final RecipeService recipeService;
+    private final IngredientService ingredientService;
 
-    public IngredientController(RecipeService recipeService) {
+    public IngredientController(RecipeService recipeService, IngredientService ingredientService) {
         this.recipeService = recipeService;
+        this.ingredientService = ingredientService;
     }
 
     @GetMapping
@@ -27,5 +30,13 @@ public class IngredientController {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(recipeId)));
 
         return "recipe/ingredient/list";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+    public String showRecipeIngredient(@PathVariable String recipeId,
+                                       @PathVariable String id, Model model){
+        model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
+        return "recipe/ingredient/show";
     }
 }
