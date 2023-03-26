910e916397bcfe8a6732e7686721d4c104950722
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-26 13:42:39 +0530

    testing DeleteById

--- src/test/java/shubh/springFramework/services/RecipeServiceImplTest.java
+++ src/test/java/shubh/springFramework/services/RecipeServiceImplTest.java
@@ -86,4 +86,19 @@ public class RecipeServiceImplTest {
         verify(recipeRepository, times(1)).findAll();
         // how many times finaAll method is being called
     }
+
+    @Test
+    public void testDeleteById() throws Exception {
+
+        //given
+        Long idToDelete = Long.valueOf(2L);
+
+        //when
+        recipeService.deleteById(idToDelete);
+
+        //no 'when', since method has void return type
+
+        //then
+        verify(recipeRepository, times(1)).deleteById(anyLong());
+    }
 }
