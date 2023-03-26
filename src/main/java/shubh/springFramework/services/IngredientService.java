6467da33ee47d7d24641af4741e6bf32c09d369f
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-26 17:15:02 +0530

    adding saveIngredientCommand in IngredientService

--- src/main/java/shubh/springFramework/services/IngredientService.java
+++ src/main/java/shubh/springFramework/services/IngredientService.java
@@ -4,4 +4,6 @@ import shubh.springFramework.commands.IngredientCommand;
 
 public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long ingredientId);
+
+    IngredientCommand saveIngredientCommand(IngredientCommand command);
 }
