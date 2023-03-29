8a8c106c7410e064ba1c4290eceb15d3803fd7a0
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-29 13:12:51 +0530

    adding mapping to add new ingredient

--- src/main/java/shubh/springFramework/controllers/IngredientController.java
+++ src/main/java/shubh/springFramework/controllers/IngredientController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import shubh.springFramework.commands.IngredientCommand;
+import shubh.springFramework.commands.RecipeCommand;
+import shubh.springFramework.commands.UnitOfMeasureCommand;
 import shubh.springFramework.services.IngredientService;
 import shubh.springFramework.services.RecipeService;
 import shubh.springFramework.services.UnitOfMeasureService;
@@ -40,6 +42,27 @@ public class IngredientController {
                                        @PathVariable String id, Model model){
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newRecipe(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
     }
 
     @GetMapping
