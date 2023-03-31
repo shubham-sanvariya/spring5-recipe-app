6c57fac5bf8eaefbe242ec0f2b9c1eb5c3b1b4b5
shubham-sanvariya <shubhamsanvariyait.com>
2023-03-31 11:45:21 +0530

    adding ImageService & it's implementation

---
+++ src/main/java/shubh/springFramework/services/ImageService.java
@@ -0,0 +1,7 @@
+package shubh.springFramework.services;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface ImageService {
+    void saveImageFile(Long recipeId, MultipartFile file);
+}
---
+++ src/main/java/shubh/springFramework/services/ImageServiceImpl.java
@@ -0,0 +1,14 @@
+package shubh.springFramework.services;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@Slf4j
+public class ImageServiceImpl implements ImageService {
+    @Override
+    public void saveImageFile(Long recipeId, MultipartFile file) {
+        log.debug("Received a file");
+    }
+}
