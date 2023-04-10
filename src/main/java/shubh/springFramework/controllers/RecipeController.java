36485819292e14f8a8561aa566e407b897c49258
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-10 14:47:32 +0530

    adding handleNotFound mapping in controller and testing by adding view & name

--- src/main/java/shubh/springFramework/controllers/RecipeController.java
+++ src/main/java/shubh/springFramework/controllers/RecipeController.java
@@ -1,9 +1,12 @@
 package shubh.springFramework.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import shubh.springFramework.commands.RecipeCommand;
+import shubh.springFramework.exceptions.NotFoundException;
 import shubh.springFramework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -50,6 +53,21 @@ public class RecipeController {
 
         recipeService.deleteById(Long.valueOf(id));
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception exception){
+
+        log.error("Handling not found exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
     }
 
 }
