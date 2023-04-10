e8095e5e94d84ab246570eacf6573db2b65876d0
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-10 16:31:24 +0530

    adding test for NumberFormatException

--- src/test/java/shubh/springFramework/controllers/RecipeControllerTest.java
+++ src/test/java/shubh/springFramework/controllers/RecipeControllerTest.java
@@ -60,6 +60,15 @@ public class RecipeControllerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
     }
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+        when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 
     @Test
     public void testGetNewRecipeForm() throws Exception {
