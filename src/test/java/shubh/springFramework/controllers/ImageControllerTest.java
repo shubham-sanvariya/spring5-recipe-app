e9b689de7545b187904955a6e81da4352aa97162
shubham-sanvariya <shubhamsanvariyait.com>
2023-04-11 12:16:37 +0530

    adding ControllerExceptionHandler for global exceptions

--- src/test/java/shubh/springFramework/controllers/ImageControllerTest.java
+++ src/test/java/shubh/springFramework/controllers/ImageControllerTest.java
@@ -38,7 +38,8 @@ public class ImageControllerTest {
         MockitoAnnotations.initMocks(this);
 
         controller = new ImageController(imageService, recipeService);
-        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setControllerAdvice(new ControllerExceptionHandler()).build();
     }
 
     @Test
@@ -99,6 +100,14 @@ public class ImageControllerTest {
         byte[] reponseBytes = response.getContentAsByteArray();
 
         assertEquals(s.getBytes().length, reponseBytes.length);
+    }
+
+    @Test
+    public void testGetImageNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/asdf/recipeimage"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
     }
 
 }
